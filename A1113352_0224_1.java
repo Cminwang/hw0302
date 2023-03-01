3510b39548346bc41cceeaec82f7f07ecbf6bf50
王希銘 <125672114+Cminwang@users.noreply.github.com>
2023-03-01 20:01:08 +0800

    Add files via upload

---
+++ A1113352_0224_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class A1113352_0224_1{
+ public static void main(String[]argv){
+  int x=0;
+ System.out.println("請輸入整數");
+ Scanner input=new Scanner(System.in);
+  x=input.nextInt();
+   if(x%2==0){
+   System.out.println("偶數");}
+   else{ System.out.println("奇數");
+    }
+}
+}
---
+++ A1113352_0224_2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class A1113352_0224_2{
+ public static void main(String[]argv){
+ float C,F;
+System.out.println("輸入攝氏溫度");
+Scanner input= new Scanner(System.in);
+C=input.nextFloat();
+F=C*9/5+32;
+System.out.println("華氏溫度為"+F+"度");
+}}
